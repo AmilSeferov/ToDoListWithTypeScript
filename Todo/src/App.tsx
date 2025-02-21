da3fbe71154ab5a0a53881a954373a5328518682
amil <huawe@HUAWEI.(none)>
2025-02-21 23:33:36 +0400

    ToDoCreate added

---
+++ Todo/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ToDoCreate from './components/ToDoCreate'
+
+function App() {
+ 
+
+  return (
+    <>
+     <ToDoCreate/>
+    </>
+  )
+}
+
+export default App
---
+++ Todo/src/components/ToDoCreate.tsx
@@ -0,0 +1,11 @@
+import '../css/ToDoCreate.css'
+function ToDoCreate() {
+  return (
+    <div>
+        <input  placeholder='Enter ToDo ...' type="text" />
+        <button>Add</button>
+    </div>
+  )
+}
+
+export default ToDoCreate
